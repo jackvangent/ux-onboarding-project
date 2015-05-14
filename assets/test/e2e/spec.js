a14ac3f81f065375207452059fa49f4c989efa61
Jack Van Gent <jackvangent1994@gmail.com>
2015-05-14 15:49:25 -0500

    Add e2e tests for ng-show and ng-hide

--- assets/test/e2e/spec.js
+++ assets/test/e2e/spec.js
@@ -1,7 +1,24 @@
-//spec.js
 describe('ux onboarding app', function() {
-	it('should have a title', function() {
-		browser.get('http://0.0.0.0:8080/#/list');
-		expect(browser.getTitle()).toEqual('User Profile');
+
+	beforeEach(function() {
+		browser.get('http://0.0.0.0:8080');
 	});
+
+	describe('tests for ng-show and ng-hide', function() {
+		it('should show user info and collapse listInfo onclick', function() {
+			element.all(by.className('userRow')).first().click();
+			expect(element.all(by.className('userRow')).first().isDisplayed()).toBeTruthy();
+			expect(element(by.className('listInfo')).isDisplayed()).toBeFalsy();
+		});
+
+		it('should close user info and show listInfo onclick of close button', function() {
+			element.all(by.className('userRow')).first().click();
+			element(by.id('minButton')).click();
+			expect(element(by.className('profileBox')).isDisplayed()).toBeFalsy();
+			expect(element(by.className('listInfo')).isDisplayed()).toBeTruthy();
+		});
+	});
+
+	//TODO: more ng-resource tests for all CRUD stuff, test forms, test filters
+
 });
